8a864723169fb1e792f45073bfd1c14f63b51f20
mariana740 <filgueirasmariana74@gmail.com>
2026-08-27 09:11:14 -0300

    criação de harquivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,47 @@
 function textoMensagemBoasVindas() {
     let nomeUsuario = prompt("Digite seu Nome Completo:");
-    if (nomeUsuario  === "") {
+    if (!nomeUsuario || nomeUsuario.trim() === "") {
         nomeUsuario = "Usuário";
     }
+
     const atual = new Date();
-    const diaSemana = ["Domingo","Segunda-Feira","Terça-Feira","Quarta-Feira","Quinta-Feira","Sexta-Feira","Sábado"];
+    const diaSemana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
     const textoSemana = diaSemana[atual.getDay()];
+
     let dia = String(atual.getDate());
     if (atual.getDate() < 10) {
-        dia = "0" + dias;
+        dia = "0" + dia; // Corrigido de 'dias' para 'dia'
     }
+
     let mes = String(atual.getMonth() + 1);
     if (atual.getMonth() + 1 < 10) {
         mes = "0" + mes;
     }
+
     const ano = atual.getFullYear();
+
     let horas = String(atual.getHours());
     if (atual.getHours() < 10) {
         horas = "0" + horas;
     }
+
     let minutos = String(atual.getMinutes());
     if (atual.getMinutes() < 10) {
         minutos = "0" + minutos;
     }
+
     const fuso = "-03:00";
-    const atualmente = ` ${textoSemana}, ${dia}/${mes}/${ano} - ${horas}:${minutos} (${fuso}) `;
-    const texto = ` Olá, ${nomeUsuario}! Hoje é ${atualmente} `;
+    const dataAtual =  ${SemanaToda}, ${dia}/${mes}/${ano} - ${horas}:${minutos} (${FusoHorario}) ` ;
+    const texto =  ` Olá, ${nomeUsuario}! Hoje é ${atualmente} ;
+
+
+
     const formularioLogout = document.querySelector("header form");
     if (formularioLogout) {
         formularioLogout.childNodes[0].textContent = texto + " ";
     }
+
     console.log(texto);
 }
-textoMensagemBoasVindas();
 
+textoMensagemBoasVindas();
